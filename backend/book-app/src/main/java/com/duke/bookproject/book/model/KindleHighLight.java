05b86d4df2ff6124e3773c2ca8bf76e8a10ef2c1
ducvotm <vtminhduc140101@gmail.com>
2025-10-21 13:28:07 +0700

    Refactor KindleHighLight: remove redundant fields, keep user/book/content

--- backend/book-app/src/main/java/com/duke/bookproject/book/model/KindleHighLight.java
+++ backend/book-app/src/main/java/com/duke/bookproject/book/model/KindleHighLight.java
@@ -2,11 +2,11 @@ package com.duke.bookproject.book.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import com.duke.bookproject.account.model.User;
+import com.duke.bookproject.book.model.Book;
 
 @Entity
 @Data
@@ -14,12 +14,18 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 public class KindleHighLight {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String author;
-    private String title;
-    private String content;
-    private Date publishDate;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "book_id", nullable = false)
+	private Book book;
+
+	private String content;
+
 }
